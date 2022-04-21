24b5754c4b4b9bbcd7b5d614b944d7ab08a1205b
Vyas Raina <sayvaniar@gmail.com>
2022-04-21 21:21:13 +0100

    evaluate

---
+++ eval.py
@@ -0,0 +1,55 @@
+'''
+Evaluate a model for synthetic data classification
+'''
+
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import sys
+import os
+import argparse
+from tools import AverageMeter, accuracy_binary, get_default_device
+from models import FFN
+import numpy as np
+from train import eval
+
+if __name__ == "__main__":
+
+    # Get command line arguments
+    commandLineParser = argparse.ArgumentParser()
+    commandLineParser.add_argument('MODEL', type=str, help='Specify model th file path')
+    commandLineParser.add_argument('ARCH', type=str, help='ffn')
+    commandLineParser.add_argument('TEST_DATA', type=str, help='path to test.npy file')
+    commandLineParser.add_argument('--B', type=int, default=16, help="Specify batch size")
+    commandLineParser.add_argument('--num_hidden_layers', type=int, default=1, help="number of hidden layers")
+    commandLineParser.add_argument('--hidden_layer_size', type=int, default=10, help="size of hidden layers")
+    args = commandLineParser.parse_args()
+
+    # Save the command run
+    if not os.path.isdir('CMDs'):
+        os.mkdir('CMDs')
+    with open('CMDs/eval.cmd', 'a') as f:
+        f.write(' '.join(sys.argv)+'\n')
+
+    # Get the device
+    device = get_default_device()
+
+    # Load the data as tensors
+    with open(args.TEST_DATA, 'rb') as f:
+        data = np.load(f)
+    x = data[:,:-1].type(torch.FloatTensor)
+    y = data[:,-1].type(torch.FloatTensor)
+
+    # Use dataloader to handle batches
+    ds = TensorDataset(x, y)
+    dl = DataLoader(ds, batch_size=args.B)
+
+    # Initialise classifier
+    model = FFN(num_hidden_layers=args.num_hidden_layers, hidden_layer_size=args.hidden_layer_size)
+    model.load_state_dict(torch.load(args.MODEL, map_location=torch.device('cpu')))
+    model.to(device)
+
+    # Criterion
+    criterion = nn.BCELoss().to(device)
+    _ = eval(dl, model, criterion, device)
