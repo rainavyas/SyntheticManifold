8821bcd76261055bef1836a0a8a528763b3322ae
Vyas Raina <sayvaniar@gmail.com>
2022-04-22 00:17:17 +0100

    fgsm

--- attack.py
+++ attack.py
@@ -11,7 +11,7 @@ import argparse
 from models import FFN
 import numpy as np
 
-def fgsm_attack(x, y, model, criterion):
+def fgsm_attack(x, y, model, criterion, epsilon):
     '''
     x: torch.Tensor [Bx3]
     y: torch.Tensor [B]
@@ -25,7 +25,7 @@ def fgsm_attack(x, y, model, criterion):
     loss = criterion(pred, y)
     loss.backward()
 
-    x_attacked = x + torch.sign(x.grad)
+    x_attacked = x + epsilon*torch.sign(x.grad)
     import pdb; pdb.set_trace()
     return x_attacked
 
@@ -38,10 +38,10 @@ if __name__ == "__main__":
     commandLineParser.add_argument('ARCH', type=str, help='ffn')
     commandLineParser.add_argument('ATTACK', type=str, help='fgsm')
     commandLineParser.add_argument('ORIG_DATA', type=str, help='path to test.npy file')
-    commandLineParser.add_argument('OUT_DATA', type=str, help='path to save attacked test.npy file')
+    commandLineParser.add_argument('OUT_DATA', type=str, help='path to dir to save attacked test.npy file')
     commandLineParser.add_argument('--num_hidden_layers', type=int, default=1, help="number of hidden layers")
     commandLineParser.add_argument('--hidden_layer_size', type=int, default=10, help="size of hidden layers")
-    commandLineParser.add_argument('--epsilon', type=float, default=10, help="attack size")
+    commandLineParser.add_argument('--epsilon', type=float, default=0.1, help="attack size")
     args = commandLineParser.parse_args()
 
     # Save the command run
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
     # Attack
     if args.ATTACK == 'fgsm':
-        x_attacked = fgsm_attack(x, y, model, criterion)
+        x_attacked = fgsm_attack(x, y, model, criterion, args.epsilon)
     
     # Save the attacked data
-    np.save(args.OUT, x_attacked.cpu().detach().numpy())
+    np.save(f'{args.OUT}/{args.ORIG_DATA[:-4]}_{args.ATTACK}_epsilon{args.epsilon}.npy', x_attacked.cpu().detach().numpy())
